52bcaa0993d0a3179e05bc3fae988f73efd9a7d5
Pedro Albuquerque <palbuquerque181@gmail.com>
2021-08-09 21:40:58 -0300

    Criação do script.js
    Criação do script que faz o neomorfismo do desafio Dia 2 funcionar.

---
+++ Desafios/Dia_2/script.js
@@ -0,0 +1,39 @@
+/* Para começarmos o neomorfismo da página, vamos declarar as variáveis necessárias. As variáveis são todos os elementos que se encontram na <div> com id switch-cnt*/
+
+
+
+
+let switchCtn = document.querySelector('#switch-cnt');
+let switchC1 = document.querySelector('#switch-c1');
+let switchC2 = document.querySelector('#switch-c2');
+let switchCircle = document.querySelectorAll('.switch_circle');
+let switchBtn = document.querySelectorAll('.switch-btn');
+let aContainer = document.querySelector('#a-container');
+let bContainer = document.querySelector('#b-container');
+let allButtons = document.querySelectorAll('.submit');
+
+let getButtons = (e) => e.preventDefault();
+
+let changeForm = (e) => {
+    switchCtn.classList.add('is-gx');
+    setTimeout(function(){
+        switchCtn.classList.remove('is-gx');
+    }, 1500);
+
+    switchCtn.classList.toggle('is-txr');
+    switchCircle[0].classList.toggle('is-txr');
+    switchCircle[1].classList.toggle('is-txr');
+
+    switchC1.classList.toggle('is-hidden');
+    switchC2.classList.toggle('is-hidden');
+    aContainer.classList.toggle('is-txl');
+    bContainer.classList.toggle('is-txl');
+    bContainer.classList.toggle('is-z200');
+};
+
+let mainF = (e) => {
+    for (var i = 0; i < allButtons.length; i++) allButtons[i].addEventListener('click', getButtons);
+    for (var i = 0; i < switchBtn.length; i++) switchBtn[i].addEventListener('click', changeForm);
+};
+
+window.addEventListener('load', mainF);
